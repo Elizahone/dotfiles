dc12e608d462e4415e0bccf55258665b09915f3e
Elizahone <elizahone@mail>
2025-01-08 17:46:44 +0800

    emacs modula config

---
+++ emacs/lisp/init-appearance.el
@@ -0,0 +1,59 @@
+(load-theme 'wombat)
+;; --------------------------------------------------------------------------------
+;; fonts
+;; --------------------------------------------------------------------------------
+
+(global-display-line-numbers-mode 1)
+
+(set-face-attribute 'default nil :font (font-spec :family "Source Code Pro"))
+  ;; 修改中文字体  设置 :size 值就无法调整大小
+(set-fontset-font "fontset-default" 'unicode (font-spec :family "KaiTi_GB2312") nil 'prepend)
+
+;; --------------------------------------------------------------------------------
+;; theme
+;; --------------------------------------------------------------------------------
+;; (use-package doom-themes
+;;   :config
+;;   (setq doom-themes-enable-bold t
+;;         doom-themes-enable-italic t)
+;;   :init (load-theme 'doom-palenight t))
+;; (use-package all-the-icons
+;;   :if (display-graphic-p)) ;; 第一次运行时, M-x all-the-icon-install-fonts 安装字体.
+
+;; (use-package all-the-icons-nerd-fonts)
+
+;; --------------------------------------------------------------------------------
+;; indent-bar
+;; --------------------------------------------------------------------------------
+
+
+;; (use-package indent-bars
+;;   :custom
+;;   (indent-bars-no-descend-string t)
+;;   (indent-bars-treesit-ignore-blank-lines-types '("module"))
+;;   (indent-bars-prefer-character t)
+;;   (indent-bars-treesit-scope '((python function_definition class_definition for_statement
+;;                                        if_statement with_statement while_statement)))
+;;   :hook ((prog-mode yaml-mode) . indent-bars-mode)
+;;   :config (require 'indent-bars-ts))
+
+;; --------------------------------------------------------------------------------
+;; doom-modeline
+;; --------------------------------------------------------------------------------
+
+;; (use-package minions
+;;   :defer t
+;;   :hook doom-modeline-mode)
+;; (use-package doom-modeline
+;;   :hook (after-init . doom-modeline-mode)
+;;   :custom
+;;   (doom-modeline-unicode-fallback t)
+;;   :config
+;;   (setq doom-modeline-height 1) ; optional
+;;   (custom-set-faces
+;;    '(mode-line ((t (:height 1.2))))
+;;    '(mode-line-active ((t (:height 0.85)))) ; For 29+
+;;    '(mode-line-inactive ((t (:height 0.85))))))
+
+
+(provide 'init-appearance)
---
+++ emacs/lisp/init-edit.el
@@ -0,0 +1,32 @@
+(delete-selection-mode t)
+
+;; --------------------------------------------------------------------------------
+;; expand-region
+;; --------------------------------------------------------------------------------
+
+(use-package expand-region
+  :bind ("C-=" . er/expand-region)
+  :config
+  (when (treesit-available-p)
+    (defun treesit-mark-bigger-node ()
+      "Use tree-sitter to mark regions."
+      (let* ((root (treesit-buffer-root-node))
+             (node (treesit-node-descendant-for-range root (region-beginning) (region-end)))
+             (node-start (treesit-node-start node))
+             (node-end (treesit-node-end node)))
+        ;; Node fits the region exactly. Try its parent node instead.
+        (when (and (= (region-beginning) node-start) (= (region-end) node-end))
+          (when-let* ((node (treesit-node-parent node)))
+            (setq node-start (treesit-node-start node)
+                  node-end (treesit-node-end node))))
+        (set-mark node-end)
+        (goto-char node-start)))
+    (add-to-list 'er/try-expand-list 'treesit-mark-bigger-node)))
+
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+
+
+(provide 'init-edit)
